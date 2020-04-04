d4f40cf01be382c0e71c5be0e4eb9904b5fcc704
Haitamrane <hassane.aamrane@gmail.com>
2020-04-04 19:26:42 +0200

    'to'

--- src/App.js
+++ src/App.js
@@ -40,10 +40,7 @@ class App extends Component {
     }
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-
-    navigator.mediaDevices.getUserMedia(constraints)
-    .then( succes )
-    .catch( failure )
+    navigator.mediaDevices.getUserMedia(constraints, succes, failure)
     }
   }
 
