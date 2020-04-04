33d49c4a52ab175b352c211b4e57dc6779e1034e
Haitamrane <hassane.aamrane@gmail.com>
2020-04-04 02:29:15 +0200

    Updates

---
+++ precache-manifest.844ea0da794c13219ffec0bd88bf9ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce0c1c3916fb492002f760d6f94b538",
+    "url": "/reactest/index.html"
+  },
+  {
+    "revision": "638fe30b6188697d528a",
+    "url": "/reactest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a18d43196547948386f9",
+    "url": "/reactest/static/js/2.8ebdf4f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactest/static/js/2.8ebdf4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638fe30b6188697d528a",
+    "url": "/reactest/static/js/main.f47e693f.chunk.js"
+  },
+  {
+    "revision": "55ef0e6306d09e826722",
+    "url": "/reactest/static/js/runtime-main.2a914a9f.js"
+  }
+]);
